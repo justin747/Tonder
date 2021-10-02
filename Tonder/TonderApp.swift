fdc36809e11cbb1cc8483052e1c4d992bfd15b78
Justin747 <justin747@gmail.com>
2021-10-01 20:13:46 -0700

    Initial Commit

---
+++ Tonder/TonderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TonderApp.swift
+//  Tonder
+//
+//  Created by Justin747 on 10/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TonderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
