1792099a14b6677f2525d58e01b25568f428660a
Justin747 <justin747@gmail.com>
2021-10-03 19:46:15 -0700

    Added components to Main View
    - Added Top HStack
    - Added Card View for swipe pictures
    - Added Bottom HStack

--- Tonder/ContentView.swift
+++ Tonder/ContentView.swift
@@ -8,11 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let cardGradient
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            //MARK: - Top HStack
+            
+            HStack {
+                
+                Button(action: {}) {
+                    Image("profile")
+                }
+                
+                Spacer()
+                
+                
+                Button(action: {}) {
+                    Image("tinder-icon")
+                        .resizable().aspectRatio(contentMode: .fit)
+                        .frame(height: 45)
+                }
+                
+                
+                Spacer()
+                
+                Button(action: {}) {
+                    Image("chats")
+                }
+            }.padding(.horizontal)
+            
+            
+            //MARK: - Card View
+            
+            ZStack {
+                Image("p0")
+                    .resizable()
+                LinearGradient(gradient: <#T##Gradient#>, startPoint: .top, endPoint: .bottom)
+            }
+            
+            
+            
+            //MARK: - Bottom HStack
+            
+            HStack(spacing: 0 ) {
+                
+                Button(action: {}) {
+                    Image("refresh")
+                }
+                Button(action: {}) {
+                    Image("dismiss")
+                }
+                Button(action: {}) {
+                    Image("super_like")
+                }
+                Button(action: {}) {
+                    Image("like")
+                }
+                Button(action: {}) {
+                    Image("boost")
+                }
+                
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
