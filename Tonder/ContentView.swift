1f0cf2852faafe635e491d56db77735edd616590
Justin747 <justin747@gmail.com>
2021-10-03 21:37:38 -0700

    Updated Card View
    - Added swipe logic
    - Added switch statement for gestures
    - Added banner for left and right swipe

--- Tonder/ContentView.swift
+++ Tonder/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-   
+    
     var body: some View {
         VStack {
             
@@ -42,11 +42,11 @@ struct ContentView: View {
             //MARK: - Card View
             
             ZStack {
-                ForEach(Card.data) { card in
+                ForEach(Card.data.reversed()) { card in
                     CardView(card: card)
                         .padding(8)
                 }
-            }
+            }.zIndex(1)
             
             
             //MARK: - Bottom HStack
@@ -83,11 +83,11 @@ struct ContentView_Previews: PreviewProvider {
 
 struct CardView: View {
     
-    let card: Card
+    @State var card: Card
     let cardGradient = Gradient(colors: [Color.black.opacity(0), Color.black.opacity(0.5)])
     
     var body: some View {
-        ZStack(alignment: .leading) {
+        ZStack(alignment: .topLeading) {
             Image(card.imageName)
                 .resizable()
             LinearGradient(gradient: cardGradient, startPoint: .top, endPoint: .bottom)
@@ -104,10 +104,57 @@ struct CardView: View {
                     
                     Text(card.bio)
                 }
-                .padding()
-                .foregroundColor(.white)
+            }
+            .padding()
+            .foregroundColor(.white)
+            
+            HStack {
+                Image("yes")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150)
+                    .opacity(Double(card.x/20 - 1))
+                
+                Spacer()
+                
+                Image("nope")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150)
+                    .opacity(Double(card.x/20 * -1 - 1))
             }
         }
         .cornerRadius(8)
+        .offset(x: card.x, y: card.y)
+        .rotationEffect(.init(degrees: card.degree))
+        
+        .gesture(
+            
+            DragGesture()
+            
+                .onChanged { value in
+                    withAnimation(.default) {
+                        card.x = value.translation.width
+                        card.y = value.translation.height
+                        card.degree = 7 * (value.translation.width > 0 ? 1 : -1 )
+                    }
+                }
+            
+                .onEnded { value in
+                    withAnimation(.interpolatingSpring(mass: 1.0, stiffness: 50, damping: 8, initialVelocity: 0)) {
+                        switch value.translation.width {
+                        case 0...200:
+                            card.x = 0; card.degree = 0; card.y = 0
+                        case let x where x > 200:
+                            card.x = 500; card.degree = 12
+                        case (-200)...(-1):
+                            card.x = 0; card.degree = 0; card.y = 0;
+                        case let x where x < 200:
+                            card.x = -500; card.degree = -12
+                        default : card.x = 0; card.y = 0
+                        }
+                    }
+                }
+        )
     }
 }
